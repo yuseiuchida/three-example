6585ef67f3404200afed9c0deb0b45b802ae9811
rq-yuseiuchida-at-321899771753 <yusei@Yuseis-MacBook-Air.local>
2022-06-26 12:23:03 +0900

    シーンとカメラを追加

--- main.js
+++ main.js
@@ -1,2 +1,37 @@
 import * as THREE from "three";
-console.log(THREE);
+
+let scene, camera, renderer;
+
+//シーン
+scene = new THREE.Scene();
+
+//カメラ
+camera = new THREE.PerspectiveCamera(
+  50,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 0, 500);
+
+//レンダラー
+renderer = new THREE.WebGLRenderer({
+  alpha: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement);
+
+renderer.render(scene, camera);
+
+//ジオメトリ作成
+let ballGeometry = new THREE.SphereGeometry(100, 64, 32);
+
+//マテリアル
+let ballMaterial = new THREE.MeshPhysicalMaterial();
+
+//メッシュ
+let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
+scene.add(ballMesh);
+
+//レンダリングする
+renderer.render(scene, camera);
